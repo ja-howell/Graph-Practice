35bd9a1f7afbc8bf0e281489a5f75841abd34767
ja-howell <42947215+ja-howell@users.noreply.github.com>
2019-08-16 20:19:28 -0500

    added test main function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 // File: main.cpp
 
 #include <iostream>
+#include "LinkedList.h"
 
 using namespace std;
 
 int main() {
+    LinkedList<int> myList;
 
+    myList.pushAt(1,1);
+    cout << myList << endl;
+    myList.pushFront(2);
+    myList.pushFront(3);
+    myList.pushBack(4);
+    cout << myList << endl;
+    myList.pushAt(1,5);
+    myList.pushAt(0,6);
+    myList.pushAt(5, 7);
+
+    cout << myList << endl;
     
     return 0;
 }// end main()
