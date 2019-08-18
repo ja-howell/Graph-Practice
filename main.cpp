6156e5e042e0959aa761921d922af445874f2832
ja-howell <42947215+ja-howell@users.noreply.github.com>
2019-08-18 10:44:34 -0500

    added test for popFront

--- main.cpp
+++ main.cpp
@@ -7,7 +7,11 @@ using namespace std;
 
 int main() {
     LinkedList<int> myList;
+    int listData;
 
+    listData = myList.popFront();
+    cout << listData << endl 
+         << myList << endl;
     myList.pushAt(1,1);
     cout << myList << endl;
     myList.pushFront(2);
@@ -17,6 +21,10 @@ int main() {
     myList.pushAt(1,5);
     myList.pushAt(0,6);
     myList.pushAt(5, 7);
+    cout << myList << endl;
+    listData = myList.popFront();
+    cout << listData << endl 
+         << myList << endl;
 
     cout << myList << endl;
     
